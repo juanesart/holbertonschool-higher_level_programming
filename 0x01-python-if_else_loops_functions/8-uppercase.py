7804536df3cceadebbd8db07cd85541c5e8e6911
juanesart <juanesartq@gmail.com>
2020-12-08 17:27:11 +0000

    task 7, 8, 9, 10

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        c = ord(c)
+        if ord('a') <= c <= ord('z'):
+            c = c - 32
+        print("{:c}".format(c), end='')
+    print()
