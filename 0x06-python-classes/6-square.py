1394cfbcd049a94f8b937fc47213e90f2ad0abca
juanesart <juanesartq@gmail.com>
2021-01-05 19:37:55 +0000

    mandatory task

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Write a Square class"""
+
+
+class Square:
+    """Addition to Square- A instance attribute position and modify my_print"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if not isinstance(self.position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(self.position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(self.position[0]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(self.position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if self.position[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if self.position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        if self.size > 0:
+            if self.position[1] > 0:
+                for newline in range(self.position[1]):
+                    print("")
+            for row in range(self.size):
+                if self.position[0] > 0:
+                    for spaces in range(self.position[0]):
+                        print(" ", end="")
+                for collumn in range(self.size):
+                    print("#", end="")
+                print("")
+        else:
+            print("")
